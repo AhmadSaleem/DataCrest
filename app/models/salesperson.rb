f481a3567f7c64b1188d445689d77431407f07c7
= <=>
2017-10-16 15:59:52 +0500

    Create relation between salespeople and wholesaler

--- app/models/salesperson.rb
+++ app/models/salesperson.rb
@@ -1,2 +1,5 @@
 class Salesperson < ApplicationRecord
+  has_one :owned_wholesaler, class_name: "Wholesaler", foreign_key: :owner_id, inverse_of: :salesperson_owner
+
+  belongs_to :wholesaler
 end
--- app/models/wholesaler.rb
+++ app/models/wholesaler.rb
@@ -1,2 +1,5 @@
 class Wholesaler < ApplicationRecord
+  has_many :salespeople, dependent: :destroy
+
+  belongs_to :salesperson_owner, class_name: "Salesperson", foreign_key: :owner_id
 end
