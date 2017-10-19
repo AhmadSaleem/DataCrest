ee5519ef32a74ccdc71e19d77b7f57bb4f768f03
= <=>
2017-10-19 10:30:17 +0500

    Create associations between agency and agent model

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,2 +1,5 @@
 class Agency < ApplicationRecord
+  has_many :agents, dependent: :destroy
+
+  belongs_to :owner, class_name: "Agent"
 end
--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,5 @@
 class Agent < ApplicationRecord
+  has_one :owned_agency, class_name: "Agency", foreign_key: :owner_id, inverse_of: :owner
+
+  belongs_to :agency, inverse_of: :agents
 end
