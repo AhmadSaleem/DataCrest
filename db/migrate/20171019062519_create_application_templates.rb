7796cee9d039a5b9c955e97368f2fb888f1f18da
= <=>
2017-10-20 13:11:00 +0500

    Update migration to remove column

--- db/migrate/20171019062519_create_application_templates.rb
+++ db/migrate/20171019062519_create_application_templates.rb
@@ -3,7 +3,6 @@ class CreateApplicationTemplates < ActiveRecord::Migration[5.1]
     create_table :application_templates do |t|
       t.references :wholesaler, null: false
       t.string     :title, null: false, unique: true
-      t.string     :keys,  array: true, default: []
 
       t.timestamps
     end
