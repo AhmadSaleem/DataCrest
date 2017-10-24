9ca71151a4dc604197fc98f4057cf2024dc3b25a
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-10-24 10:35:09 +0500

    Mount wholesaler to upload logo

--- app/models/wholesaler.rb
+++ app/models/wholesaler.rb
@@ -3,6 +3,8 @@ class Wholesaler < ApplicationRecord
   has_many :agent_wholesalers
   has_many :agents, through: :agent_wholesalers
 
+  mount_uploader :logo, LogoUploader
+
   belongs_to :owner, class_name: "Salesperson"
 
   validates :title, presence: true
