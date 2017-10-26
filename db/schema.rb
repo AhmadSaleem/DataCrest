94cab38bd8d561aa856a7292180649db97af9140
Talha Junaid <=>
2017-10-26 17:09:30 +0500

    Add model application and its table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026111840) do
+ActiveRecord::Schema.define(version: 20171026120023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,15 @@ ActiveRecord::Schema.define(version: 20171026111840) do
     t.index ["invited_by_id"], name: "index_agents_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_agents_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_agents_on_reset_password_token", unique: true
+  end
+
+  create_table "applications", force: :cascade do |t|
+    t.bigint "wholesaler_id"
+    t.string "title", null: false
+    t.string "model_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wholesaler_id"], name: "index_applications_on_wholesaler_id"
   end
 
   create_table "clients", force: :cascade do |t|
