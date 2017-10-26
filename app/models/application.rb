1914644fe758781931959b5eb92a0239d49726df
Talha Junaid <=>
2017-10-26 17:34:11 +0500

    Create association between application adn jimcor_dwelling_application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,5 @@
 class Application < ApplicationRecord
+  has_many :jimcor_dwelling_applications
+
   belongs_to :wholesaler
 end
--- app/models/jimcor_dwelling_application.rb
+++ app/models/jimcor_dwelling_application.rb
@@ -3,4 +3,5 @@ class JimcorDwellingApplication < ApplicationRecord
 
   belongs_to :client
   belongs_to :agent
+  belongs_to :application
 end
