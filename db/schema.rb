a32957e425b0ddf139200c070f8d6380d508127d
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-10-30 16:21:40 +0500

    Add percentage_of_completion field to jimcor_dwelling_applications.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027080734) do
+ActiveRecord::Schema.define(version: 20171030094914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,6 +205,7 @@ ActiveRecord::Schema.define(version: 20171027080734) do
     t.string "nearby_ocean_remarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "percentage_of_completion", default: 0
     t.index ["agent_id"], name: "index_jimcor_dwelling_applications_on_agent_id"
     t.index ["client_id"], name: "index_jimcor_dwelling_applications_on_client_id"
   end
