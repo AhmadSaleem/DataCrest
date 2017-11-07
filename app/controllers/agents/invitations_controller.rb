590932ae8da45b5da6a4d697c84927f6abb53d4c
Talha Junaid <=>
2017-11-07 15:31:10 +0500

    Add agency_fields to agent_invitation edit page

--- app/controllers/agents/invitations_controller.rb
+++ app/controllers/agents/invitations_controller.rb
@@ -2,7 +2,7 @@ class Agents::InvitationsController < Devise::InvitationsController
   before_action :set_resource, only: [:update]
 
   def update
-    @agency = self.resource.owned_agency || self.resource.create_owned_agency(title: params[:title])
+    @agency = self.resource.owned_agency
     super
     if resource.invitation_accepted_at?
       resource.wholesalers << resource.invited_by.wholesaler
@@ -15,11 +15,13 @@ class Agents::InvitationsController < Devise::InvitationsController
   private
 
     def update_resource_params
-      params.require(:agent).permit(:password, :password_confirmation, :invitation_token)
+      params.require(:agent).permit(:password, :password_confirmation, :invitation_token,
+                                    owned_agency_attributes: [:id, :title, :city, :zip_code, :state, :website, :address_1,
+                                    :address_2, :logo])
     end
 
     def set_resource
-      redirect_to accept_agent_invitation_url(invitation_token: update_resource_params[:invitation_token]), alert: "Agency cannot be blank." unless params[:title].present?
+      redirect_to accept_agent_invitation_url(invitation_token: update_resource_params[:invitation_token]), alert: "Agency must exist" unless params[:agent][:owned_agency_attributes].present?
       self.resource = Agent.find_by_invitation_token(update_resource_params[:invitation_token], true)
     end
 
