015c0cf8345e517b27f83a418362b23a45eb9aca
Talha Junaid <=>
2017-11-09 15:30:24 +0500

    Add signup for client

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -1,10 +1,24 @@
 class Clients::RegistrationsController < Devise::RegistrationsController
 
   def new
-    redirect_to new_client_session_path
+    super
+  end
+
+  def create
+    client = Client.find_by(email: sign_up_params[:email])
+    if client.present? && client.invitation_sent_at? && !client.invitation_accepted_at?
+      redirect_to new_client_registration_path, alert: "Please check your email you are already invited or ask the agent to reinvite you."
+    else
+      super
+      UserMailer.welcome(resource.email).deliver_later if resource.persisted?
+    end
   end
 
   private
+
+    def sign_up_params
+      params.require(:client).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 
     def account_update_params
       params.require(:client).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
