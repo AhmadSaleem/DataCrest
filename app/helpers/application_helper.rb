5f2fea57fc24c8a03f02440f4c4256113a7fab28
Colin Ambler <colin@nxtsports.com>
2017-11-09 06:34:06 -0500

    re-arrange temples nearly to proper structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_datetime(datetime)
     datetime.strftime("%B %d, %Y %H:%M:%S")
   end
+
+  def dashboard?
+    controller_path.split('/').first == 'dashboard'
+  end
 end
