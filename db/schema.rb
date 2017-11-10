d8e3e49e191777a47917536ec446fd8bd4cdca6b
Talha Junaid <=>
2017-11-10 19:13:24 +0500

    Add column profile_picture to salesperson

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110122700) do
+ActiveRecord::Schema.define(version: 20171110140519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -270,6 +270,7 @@ ActiveRecord::Schema.define(version: 20171110122700) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.json "profile_picture"
     t.index ["email"], name: "index_salespeople_on_email", unique: true
     t.index ["invitation_token"], name: "index_salespeople_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_salespeople_on_invitations_count"
