7a760cc257295560598aec4bc249b15c38dd2f4f
Colin Ambler <colin@nxtsports.com>
2017-11-11 17:41:11 -0500

    clean up header and footer links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,14 @@ module ApplicationHelper
     datetime.strftime("%B %d, %Y %H:%M:%S")
   end
 
+  def render_brand_website
+    'http://www.mydatacrest.com'
+  end
+
+  def render_copyright_text
+    ("&copy; <script>document.write(new Date().getFullYear())</script> " + link_to(render_brand_name + ', Inc', render_brand_website).to_s + ' all rights reserved').html_safe
+  end
+
   def current_user
     current_salesperson || current_agent || current_client
   end
