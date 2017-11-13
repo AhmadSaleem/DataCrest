67703671012e04e8a5fc62a4eda7ea812b73535e
Ahmad Saleem <ahmadsaleem93@gmail.com>
2017-11-14 01:49:53 +0500

    Fix layout issues.

--- app/controllers/agents/registrations_controller.rb
+++ app/controllers/agents/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Agents::RegistrationsController < Devise::RegistrationsController
+  layout "dashboard", only: [:edit, :update]
 
   def create
     super
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,23 +15,11 @@ module ApplicationHelper
     current_salesperson || current_agent || current_client
   end
 
-  def dashboard?
-    controller_path.split('/').first == 'dashboard' || edit_profile?
-  end
-
-  def edit_profile?
-    (controller_name.in?(['registrations']) && action_name.in?(['edit', 'update']))
-  end
-
   def new_registration?
     (controller_name.in?(['registrations']) && action_name.in?(['new', 'create']))
   end
 
   def new_session?
     (controller_name.in?(['sessions']) && action_name.in?(['new', 'create']))
-  end
-
-  def page?
-    not dashboard?
   end
 end
