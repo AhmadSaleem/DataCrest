5fb0af08d89ce0a0efd0db409dc17d79454c6802
Talha Junaid <=>
2017-11-14 11:58:12 +0500

    Refactor client signin in modal

--- app/controllers/clients/sessions_controller.rb
+++ app/controllers/clients/sessions_controller.rb
@@ -2,7 +2,7 @@ class Clients::SessionsController < Devise::SessionsController
   def create
     @user = Client.find_by(email: params[:client][:email])
     if @user && @user.valid_password?(params[:client][:password])
-      sign_in(@user.class.name.downcase, @user)
+      sign_in("client", @user)
       flash[:notice] = "Logged in successfully"
     else
       flash.now[:alert] = "Email or password is incorrect."
