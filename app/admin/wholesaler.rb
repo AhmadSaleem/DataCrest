78441e0f9c66cbf706dbe90da0212a4dc94ccad3
Talha Junaid <=>
2017-11-15 18:45:28 +0500

    Add model wholesaler in active_admin

---
+++ app/admin/wholesaler.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Wholesaler do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  permit_params :owner_id, :title, :logo, :website, :address_1, :address_2, :city, :state, :zip_code
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
