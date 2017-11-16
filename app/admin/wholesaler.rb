3544487952ba5a79054829a2b79331e6a1de0295
Talha Junaid <=>
2017-11-16 18:00:28 +0500

    Change index view page for wholesaler.

--- app/admin/wholesaler.rb
+++ app/admin/wholesaler.rb
@@ -3,13 +3,16 @@ ActiveAdmin.register Wholesaler do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
   permit_params :owner_id, :title, :logo, :website, :address_1, :address_2, :city, :state, :zip_code
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+  index do
+    selectable_column
+    column :title
+    column :owner
+    column :website do |wholesaler|
+      link_to(wholesaler.website, "http://#{wholesaler.website}", target: :blank) if wholesaler.website.present?
+    end
+    column :company_address
+    actions
+  end
 
 end
