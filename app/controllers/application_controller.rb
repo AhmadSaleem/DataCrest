00df4d1c1ed05d6165530b991d44fd3e4aa49fd5
Talha Junaid <=>
2017-11-16 19:38:59 +0500

    change string comparison to is_a? function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def after_sign_in_path_for(resource)
-      if resource.model_name == "AdminUser"
+      if resource.is_a?(AdminUser)
         admin_dashboard_path
       else
         session[:application_url] || dashboard_path
