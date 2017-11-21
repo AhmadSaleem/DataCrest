c1d8bb59d164e9948802b2e05378cf7438d47274
Talha Junaid <=>
2017-11-21 10:46:38 +0500

    Change policy_number in jimcor_applications.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121051807) do
+ActiveRecord::Schema.define(version: 20171121053553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,7 +137,7 @@ ActiveRecord::Schema.define(version: 20171121051807) do
     t.string "applicant_address"
     t.string "applicant_phone_no"
     t.string "applicant_previous_carrier"
-    t.integer "policy_number"
+    t.string "policy_number"
     t.string "policy_period"
     t.float "peril_deductible"
     t.float "wind_hail_deductible"
